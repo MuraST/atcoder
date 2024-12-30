ad84be5429b5c29a4a4a4e839de7af8acb5ffca1
MuraST <takoyaki7745@gmail.com>
2024-12-30 15:21:00 +0900

    update: bundle.shを引数に対応

--- config/bundle.sh
+++ config/bundle.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 LIBRARY_PATH="/mnt/c/Users/takoy/vscode/atcoder/library"
-CODE_FILE="main.cpp"
-OUTPUT_FILE="main.bundle.cpp"
+TMP_FILE="main.bundle.cpp"
+if [ -z "$1" ]; then
+  CODE_FILE="main.cpp"
+else
+  CODE_FILE="$1"
+fi
 
 oj-bundle "$CODE_FILE" -I "$LIBRARY_PATH" | \
 grep -v '^#line ' | \
 grep -v '^#pragma once' | \
-cat -s > "$OUTPUT_FILE"
-cat -s "$OUTPUT_FILE" > "$CODE_FILE"
-rm "$OUTPUT_FILE"
+cat -s > "$TMP_FILE"
+cat -s "$TMP_FILE" > "$CODE_FILE"
+rm "$TMP_FILE"
